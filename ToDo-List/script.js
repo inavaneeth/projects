4d9b6866dc419ba7962006c637e8ccd389881a8b
inavaneeth <navaneeth6118@gmail.com>
2025-09-21 16:06:20 +0530

    Created JS

---
+++ ToDo-List/script.js
@@ -0,0 +1,40 @@
+const addBtn = document.getElementById("addBtn");
+const taskinput = document.getElementById("taskinput");
+const list = document.getElementById("list");
+
+function addTask() {
+    const taskText = taskinput.value.trim();
+    if (taskText === "") {
+        alert("Please enter a task!");
+        return;
+    }
+    const li = document.createElement("li");
+
+    const span = document.createElement("span");
+    span.textContent = taskText;
+
+    const delBtn = document.createElement("delBtn");
+    delBtn.textContent = "Delete";
+    delBtn.classList.add("delete-btn")
+
+    span.addEventListener("click", () => {
+        li.classList.toggle("completed");
+    });
+
+    delBtn.addEventListener("click", () => {
+        li.remove();
+    });
+
+    li.appendChild(span);
+    li.appendChild(delBtn);
+    list.appendChild(li);
+
+    taskinput.value = "";
+}
+addBtn.addEventListener("click", addTask);
+
+taskinput.addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+        addTask();
+    }
+});
